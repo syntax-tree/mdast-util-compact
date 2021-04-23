583deff797ecc2fcc0e518657ee20c8f5995c533
Titus Wormer <tituswormer@gmail.com>
2021-04-23 17:59:07 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+/**
+ * @typedef {import('unist').Node} Node
+ * @typedef {import('unist-util-visit').Visitor<Node>} Visitor
+ */
+
 import {visit} from 'unist-util-visit'
 
-// Make an mdast tree compact by merging adjacent text nodes.
+/**
+ * Make an mdast tree compact by merging adjacent text nodes.
+ *
+ * @template {Node} T
+ * @param {T} tree
+ * @returns {T}
+ */
 export function compact(tree) {
   visit(tree, visitor)
 
   return tree
 
+  /** @type {Visitor} */
   function visitor(child, index, parent) {
     var siblings = parent ? parent.children : []
     var previous = index && siblings[index - 1]
@@ -16,10 +28,12 @@ export function compact(tree) {
       (child.type === 'text' || child.type === 'blockquote')
     ) {
       if (child.value) {
+        // @ts-ignore must be text.
         previous.value += child.value
       }
 
       if (child.children) {
+        // @ts-ignore must be block quote.
         previous.children = previous.children.concat(child.children)
       }
 
